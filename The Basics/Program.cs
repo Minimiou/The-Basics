76d82c789cfd91e2920b511ee1b0ad6ac2509016
Minimiou <akhalife2003@gmail.com>
2020-09-17 13:26:09 -0400

    I finished Part 1

--- The Basics/Program.cs
+++ The Basics/Program.cs
@@ -10,10 +10,84 @@ namespace The_Basics
     {
         static void Main(string[] args)
         {
+            string aFriend = "Bill";
+            string firstFriend = "Mammon";
+            string secondFriend = "Sage";
+            string greeting = "   Hello World!              ";
+            string trimmedGreeting = greeting.TrimStart();
+            string sayHello = "Hello World";
+            string songLyric = "You say goodbye, and I say hello";
+            string songLyrics = "Those poor unfortunate souls!";
+            string schoolCheer;
+            string firstName = "Aya";
+            string faveMovie = "The Matrix";
 
             Console.WriteLine("Hello World");
 
+            Console.WriteLine("Hello " + aFriend);
+            aFriend = "Maira";
+            Console.WriteLine($"Hello {aFriend}");
+
+            Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+            Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters");
+            Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters");
+
+            Console.WriteLine($"[{greeting}]");
+
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.TrimEnd();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.Trim();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            Console.WriteLine(sayHello);
+
+            sayHello = sayHello.Replace("Hello", "Greetings");
+            Console.WriteLine(sayHello);
+
+            Console.WriteLine(sayHello.ToUpper());
+            Console.WriteLine(sayHello.ToLower());
+
+            Console.WriteLine(songLyrics.Contains("unfortunate"));
+            Console.WriteLine(songLyrics.Contains("Eye"));
+
+            Console.WriteLine(songLyrics.StartsWith("You"));
+            Console.WriteLine(songLyrics.StartsWith("goodbye"));
+
+            Console.WriteLine(songLyrics.EndsWith("hello"));
+            Console.WriteLine(songLyrics.EndsWith("goodbye"));
+
+            Console.WriteLine(firstFriend.Length);
+
+            schoolCheer = "programmers are the best!";
+            schoolCheer = schoolCheer.ToUpper();
+            Console.WriteLine(schoolCheer);
+
+
+            Console.WriteLine("   A        K    K");
+            Console.WriteLine(" A   A      K   K");
+            Console.WriteLine("A     A     K  K");
+            Console.WriteLine("A A A A     KK");
+            Console.WriteLine("A     A     K  K");
+            Console.WriteLine("A     A     K   K");
+            Console.WriteLine("A     A     K    K");
+
+
+            Console.WriteLine($"Hello, my name is {firstName} and my favorite movie is {faveMovie}.".ToLower());
+            faveMovie = faveMovie.ToUpper();
+            Console.WriteLine(faveMovie);
+            Console.WriteLine(faveMovie.Contains("THE"));
+            faveMovie = faveMovie.Replace("A", "@");
+            faveMovie = faveMovie.Replace("E", "3");
+            Console.WriteLine(faveMovie);
+
             Console.ReadLine();
+
+
+
 
         }
     }
